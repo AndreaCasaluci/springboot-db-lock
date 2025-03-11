3b72628c89da6f5472b4922f34e9bfbe457d513f
andrea.casaluci <casaluci10@gmail.com>
2025-03-11 14:43:02 +0100

    Initial commit

---
+++ src/test/java/org/andrea/springbootdblock/SpringbootDbLockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andrea.springbootdblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDbLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
